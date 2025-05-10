1ba8ffb3c83a0d0001e807cc5af0fc7d5ea45114
iankendoit <iankencruz@gmail.com>
2025-05-11 03:37:42 +1000

    feat: add frontend base blocks

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { PUBLIC_API_URL } from '$env/static/public';
+
+	let pingMessage = $state('Connecting...');
+
+	$effect(() => {
+		(async () => {
+			const res = await fetch(`${PUBLIC_API_URL}/ping`);
+			const json = await res.json();
+			pingMessage = json.message;
+		})();
+	});
+
+	console.log(PUBLIC_API_URL); // http://localhost:3000/api
+</script>
+
+<h1 class="text-2xl font-bold">{pingMessage}</h1>
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,7 +3,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			// Adjust this to wherever you want the Go backend to serve from
+			pages: '../backend/static',
+			assets: '../backend/static',
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: ''
+		}
+	}
 };
 
 export default config;
